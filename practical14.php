c6becad891a6b4abb3cc307f69ea8e666fbc755d
falguni3389 <falgunidave896@gmail.com>
2025-08-29 13:25:17 +0400

    Add files via upload

---
+++ practical14.php
@@ -0,0 +1,23 @@
+<?php
+// Define three integer variables
+$num1 = 10;
+$num2 = 20;
+$num3 = 30;
+
+// Calculate sum
+$sum = $num1 + $num2 + $num3;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sum of Three Numbers</title>
+</head>
+<body>
+    <h1>PHP Sum Example</h1>
+    <p>First Number: <?= $num1 ?></p>
+    <p>Second Number: <?= $num2 ?></p>
+    <p>Third Number: <?= $num3 ?></p>
+    <p>Sum of three numbers is: <?= $sum ?></p>
+</body>
+</html>
