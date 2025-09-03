03d7a27aa52fb3d48e2f8fe5bf107cbc6d403481
falguni3389 <falgunidave896@gmail.com>
2025-09-03 13:25:32 +0400

    Add files via upload

---
+++ practical30.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr=["India" => "utter pradesh", "Pakistan" =>"chaina", "america" =>"Tokyo","afganistan" =>"Beijing",];
+
+
+foreach($arr as $key=> $value){
+    echo "<pre>";
+       echo "key=$key"."value= $value";
+        echo "</pre>";
+}
+
+
+?>
