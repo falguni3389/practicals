e1672fdcde91d303654e256f85cd4464865ce029
falguni3389 <falgunidave896@gmail.com>
2025-09-03 13:08:39 +0400

    Add files via upload

---
+++ practical25.php
@@ -0,0 +1,14 @@
+<?php
+   $arr=[65,66,67,68,69,70];
+   $max=max($arr);
+    $min=min($arr);
+
+    echo "maximum number is $max";
+    echo "<br>";
+    echo "minimum number is $min";
+    echo "<br>";
+
+
+
+
+?>
