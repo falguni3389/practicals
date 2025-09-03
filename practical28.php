93c367bb1704ea71e5fef9ed3f616ad673a5bf3e
falguni3389 <falgunidave896@gmail.com>
2025-09-03 13:23:56 +0400

    Add files via upload

---
+++ practical28.php
@@ -0,0 +1,11 @@
+<?php
+    $arr=[10,30,50,80,90];
+    $find=1;
+    if(in_array($find,$arr)){
+        echo "1 exist in this array";
+    }  else{
+        echo "1 does not exist in this array";
+    }
+
+
+?>
